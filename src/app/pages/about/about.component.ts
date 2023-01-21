9482908ded5ad2475f9be9f06d2b7f8042e710d5
Nicolás Arias <josue.arias@epn.edu.ec>
2023-01-20 23:31:15 -0500

    add scroll time in about page

--- src/app/pages/about/about.component.ts
+++ src/app/pages/about/about.component.ts
@@ -8,20 +8,25 @@ import {ActivatedRoute} from "@angular/router";
 })
 export class AboutComponent {
 
+  timeScroll: number = 0
+
   constructor(private activeRoute: ActivatedRoute) {
   }
 
   ngAfterViewChecked(): void {
-    this.activeRoute.params.subscribe(param => {
-      const id = param['section']
-      if (id) {
-        const yOffset = -90;
-        const element = document.getElementById(id);
-        if (element && element.getBoundingClientRect().top + yOffset > 0) {
-          window.scrollTo({top: element.getBoundingClientRect().top + yOffset, behavior: 'smooth'});
+    this.timeScroll = this.timeScroll + 1
+    if (this.timeScroll < 3) {
+      this.activeRoute.params.subscribe(param => {
+        const id = param['section']
+        if (id) {
+          const yOffset = -90;
+          const element = document.getElementById(id);
+          if (element && window.pageYOffset == 0) {
+            window.scrollTo({top: element.getBoundingClientRect().top + yOffset, behavior: 'smooth'});
+          }
         }
-      }
-    })
+      })
+    }
   }
 
 }
